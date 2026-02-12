a3f6f099492f188903eb8666577f72e5ab11833c
KonradDanielewski <54865575+KonradDanielewski@users.noreply.github.com>
2026-02-12 19:24:54 +0100

    add app opening from terminal

---
+++ deepecohab/__main__.py
@@ -0,0 +1,6 @@
+import webbrowser
+from deepecohab.app.app import app
+
+if __name__ == "__main__":
+	webbrowser.open_new("http://127.0.0.1:8050/")
+	app.run(debug=False, port=8050)
--- deepecohab/app/app.py
+++ deepecohab/app/app.py
@@ -47,6 +47,3 @@ app.layout = html.Div(
 		),
 	]
 )
-
-if __name__ == "__main__":
-	app.run(debug=True, port=8050)
